f099b8746d35ae482e92435223a1f58d05241cb1
Arafat <yeamin@sheba.xyz>
2021-10-16 14:19:11 +0600

    [UPDATE] #get imgur secrets

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="ui secondary pointing menu">
+            <a class="item active">
+                Image Storage
+            </a>
+
+            <div class="right menu">
+                <a class="ui item">
+                Logout
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
