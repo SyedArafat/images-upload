7571b7a78c134ce82eafc35d2b833ddf17ee81ea
Arafat <yeamin@sheba.xyz>
2021-10-18 21:17:59 +0600

    [#update] vuex started

---
+++ src/store/modules/auth.js
@@ -0,0 +1,18 @@
+ const state = {
+    token: null,
+ };
+
+ const getters = {
+    isLoggedIn: state => !!state.token
+    
+ };
+
+ const actions = {
+
+ };
+
+ const mutations = {
+      setToken: (state, token) => {
+         state.token = token;
+      }
+ };
